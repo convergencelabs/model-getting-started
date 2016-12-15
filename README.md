18085f986fed1113017d3febd03fe59f36ec4504
Michael MacFadden <michael@convergencelabs.com>
2016-12-15 00:06:09 -0800

    Initial check in.

---
+++ README.md
@@ -0,0 +1,16 @@
+# Convergence Getting Started Example
+
+The project illustrates a basic example of working with a RealTimeModel. The example requires you to have a Convergence account and at least on Domain with at least one configured user.
+
+
+## Preparation
+1. Log into the Convergence Admin Console.
+1. Create or select the domain you wish to use for the example.
+1. Create or select a domain user to use to connect.
+1. Not the domain url and username and password of the domain user.
+
+## Working with the Example
+1. Fork or clone the repository.
+1. Copy the config.example.js file to config.js
+1. Update the config.js file to match the domain and domain user information.
+1. Open the index.html in your web browser.
---
+++ example.js
@@ -0,0 +1,100 @@
+// Connect to the domain, as the domain user.
+ConvergenceDomain.connect(url, username, password)
+  .then(initApp)
+  .catch(function (error) {
+    console.log("Could not connect: " + error);
+  });
+
+/**
+ * Initializes the application after connecting by opening a model.
+ *
+ * @param {ConvergenceDomain} domain The connected domain.
+ */
+function initApp(domain) {
+  var modelService = domain.models();
+  modelService.open("example", "getting-started", function () {
+    // This method gets called if the model doesn't exist to set the initial data.
+    return {text: "Hello World"};
+  })
+  .then(initModel)
+  .catch(function (error) {
+    console.log("Could not open model: " + error);
+  });
+}
+
+
+/**
+ * Initializes model once the model is open.
+ *
+ * @param {RealTimeModel} model The connected domain.
+ */
+function initModel(model) {
+  var stringModel = model.elementAt("text");
+  var textArea = document.getElementById("textarea");
+  bindTextArea(textArea, stringModel);
+
+  // Set the initial value of the text area to the value in the model.
+  textArea.value = stringModel.value();
+}
+
+
+/**
+ * A utility method to bind a text area to the real time model.  This is
+ * available as a sparate utility, but is shown here for completeness.
+ *
+ * @param {HTMLTextAreaElement} textArea The text area element to bind too.
+ * @param {RealTimeString} stringModel The real time element that holds the text.
+ */
+function bindTextArea(textArea, stringModel) {
+
+  //
+  // Listen to the text area and send outgoing events.
+  //
+  textArea.addEventListener("input", function () {
+    var oldval = stringModel.value();
+    var newval = textArea.value;
+
+    var commonEnd = 0;
+    var commonStart = 0;
+    if (oldval === newval) {
+      return;
+    }
+
+    while (oldval.charAt(commonStart) === newval.charAt(commonStart)) {
+      commonStart++;
+    }
+
+    while (oldval.charAt(oldval.length - 1 - commonEnd) ===
+    newval.charAt(newval.length - 1 - commonEnd) &&
+    commonEnd + commonStart < oldval.length &&
+    commonEnd + commonStart < newval.length) {
+      commonEnd++;
+    }
+
+    // Characters were removed.
+    if (oldval.length !== commonStart + commonEnd) {
+      stringModel.remove(commonStart, oldval.length - commonStart - commonEnd);
+    }
+
+    // Characters were added
+    if (newval.length !== commonStart + commonEnd) {
+      stringModel.insert(commonStart, newval.slice(commonStart, (newval.length - commonEnd)));
+    }
+  }, false);
+
+  //
+  // Listen for when other users change the value and update the text area
+  //
+  stringModel.on("insert", function (event) {
+    var oldVal = textArea.value;
+    textArea.value = oldVal.substring(0, event.index) +
+      event.value +
+      oldVal.substring(event.index, oldVal.length);
+  });
+
+  stringModel.on("remove", function (event) {
+    var oldVal = textArea.value;
+    textArea.value = oldVal.substring(0, event.index) +
+      oldVal.substring(event.index + event.value.length, oldVal.length);
+  });
+}
