0df37640c9512bb3624571020c7cd5d330206102
Michael MacFadden <michael@convergencelabs.com>
2017-01-11 14:10:40 -0800

    Simplified the comments to pair down the bulk of the file.

--- example.js
+++ example.js
@@ -1,14 +1,11 @@
-// Connect to the domain, as the domain user.
+// 1. Connect to the domain anonymously.
 Convergence.connectAnonymously(DOMAIN_URL)
   .then(initApp)
   .catch(function (error) {
     console.log("Could not connect: " + error);
   });
 
-/**
- * Initializes the application after connecting by opening a model.
- * @param {ConvergenceDomain} domain The connected domain.
- */
+// 2. Initializes the application after connecting by opening a model.
 function initApp(domain) {
   var modelService = domain.models();
   modelService.open("example", "getting-started", function () {
@@ -21,10 +18,7 @@ function initApp(domain) {
   });
 }
 
-/**
- * Initializes the model once the model is open.
- * @param {RealTimeModel} model The connected domain.
- */
+// 3. Initializes the model once the model is open.
 function initModel(model) {
   var stringModel = model.elementAt("text");
   var textArea = document.getElementById("textarea");
