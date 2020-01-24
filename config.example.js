0fadfe2c5f237ae6b9b8e86c239a7fd3742ece74
Michael MacFadden <michael@convergencelabs.com>
2020-01-24 06:54:23 -0800

    Updated to reflect the omnibus container.

--- config.example.js
+++ config.example.js
@@ -2,7 +2,7 @@
 // This file configures the domain url that is used to connect to the Convergence service.
 //
 // By default, it is configured to use the default options in the Convergence Development Edition
-// (https://hub.docker.com/r/convergencelabs/convergence-de)
+// (https://hub.docker.com/r/convergencelabs/convergence-omnibus)
 // 
 // These are the relevant parts of the URL: http://<host>:<host-port>/api/realtime/<namespace>/<domainId>
 var DOMAIN_URL = "http://localhost:8000/api/realtime/convergence/default";
