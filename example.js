8e9c00bd4d6476ab8bf043814a57af3fdf0db42a
Michael MacFadden <michael@convergencelabs.com>
2020-03-30 18:48:28 -0500

    Update example.js
    Updated for more recent JS syntax.

--- example.js
+++ example.js
@@ -1,28 +1,28 @@
 // 1. Connect to the domain anonymously.
 Convergence.connectAnonymously(DOMAIN_URL)
   .then(initApp)
-  .catch(function (error) {
+  .catch((error) => {
     console.log("Could not connect: " + error);
   });
 
 // 2. Initializes the application after connecting by opening a model.
 function initApp(domain) {
-  var modelService = domain.models();
+  const modelService = domain.models();
   modelService.openAutoCreate({
     collection: "example",
     id: "getting-started",
     data: { text: "Hello World" }
   })
   .then(initModel)
-  .catch(function (error) {
+  .catch((error) => {
     console.log("Could not open model: " + error);
   });
 }
 
 // 3. Initializes the model once the model is open.
 function initModel(model) {
-  var stringModel = model.elementAt("text");
-  var textArea = document.getElementById("textarea");
+  const stringModel = model.elementAt("text");
+  const textArea = document.getElementById("textarea");
 
   // Sets the value of the text area and performs a two-way-binding.
   ConvergenceInputElementBinder.bindTextInput(textArea, stringModel);
